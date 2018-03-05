479918b145cdf3cb58f9455de2060e833d796829
abhijithvijayan <crazyzycho@protonmail.com>
2018-03-05 22:12:27 +0530

    added .cpp file

---
+++ firstCPP.cpp
@@ -0,0 +1,10 @@
+/* This program prints hello World in C++ language */
+
+#include<iostream>      // .h is not needed
+
+using namespace std;    //for cout std definition which is in namespace file
+
+int main() {
+  cout<<"Hello World!";
+  return 0;
+}
